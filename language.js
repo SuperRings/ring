b9b527254de523c72fe2a354f02f650beaeac6b6
zyl <zyl@qq.com>
2023-04-09 20:01:42 +0800

    Signed-off-by: zyl <zyl@qq.com>

---
+++ language.js
@@ -0,0 +1,13 @@
+var title= ['节奏游戏', 'Rhythm game.'];
+var title1= ['3D下落式创意节奏游戏', '3D Falling Creative Rhythm Game'];
+
+var interest_one= ['完全自由.', 'All free!'];
+var data1= ['为您呈现免费游戏,希望您在游戏中快乐.所有曲目都可以免费游玩.', 'Show you free games, and hope you are happy in the game.All tracks can be played for free.'];
+
+var interest_two= ['新的Note-RingNotes', 'New Note-RingNotes.'];
+var data2= ['您需要新的操作方式才能完成', 'Multi-finger operation may be required.'];
+
+var interest_three= ['新的剧情', 'New Tale'];
+var data3= ['Ring的是善意的还是恶意的(正在规划)', ' The Ring is kindness or malice?(Planning...)'];
+
+var datao= ['The Ring.', 'The ...'];
