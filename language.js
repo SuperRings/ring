6d13e48f2a170e557625a0eabeadd8ddcfb6439a
zyl <zyl@qq.com>
2024-03-14 21:17:40 +0800

    Signed-off-by: zyl <zyl@qq.com>

--- language.js
+++ language.js
@@ -5,10 +5,10 @@ var interest_one= ['完全自由.', 'All free!'];
 var data1= ['为您呈现免费游戏,希望您在游戏中快乐.所有曲目都可以免费游玩.', 'Show you free games, and hope you are happy in the game.All tracks can be played for free.'];
 
 var interest_two= ['新的Note-RingNotes', 'New Note-RingNotes.'];
-var data2= ['您需要新的操作方式才能完成', 'Multi-finger operation may be required.'];
+var data2= ['...', 'Multi-finger operation may be required.'];
 
 var interest_three= ['新的剧情', 'New Tale'];
-var data3= ['Ring是善意的还是恶意的(正在规划...)', ' The Ring is kindness or malice?(Planning...)'];
+var data3= ['(正在规划...)', ' The Ring is kindness or malice?(Planning...)'];
 
 var version= ['V0.1', 'V0.1'];
 var datao= ['The Ring.', 'The ...'];
